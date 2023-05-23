5e95f28c7dbee2994e2b89c8420d6371b6aadca4
br1anlee <joonmolee3@gmail.com>
2023-05-23 09:33:26 -0700

    Deleted Maps and MapDetail components

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ import Agents from './components/pages/Agents';
 import axios from 'axios'
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
-import Map from './components/pages/Map';
 import Agent from './components/pages/Agent';
 import Weapons from './components/pages/Weapons';
 import Weapon from './components/pages/Weapon';
@@ -75,11 +74,6 @@ useEffect(() => {
         <Route 
           path = '/maps'
           element = {<Maps gameMaps={gameMaps}/>}
-        />
-
-        <Route 
-          path = '/maps/:id'
-          element = {<Map />}
         />
 
         <Route 
